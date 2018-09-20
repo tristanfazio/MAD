146c9e732f2e9d6780de94bbfd9f0aaf308555ce
Tristan Fazio <2tfazio@iinet.net.au>
2018-09-20 09:26:54 +0800

    Prac03 changes

--- Prac03/app/src/main/java/tfazio/prac03/MainActivity.java
+++ Prac03/app/src/main/java/tfazio/prac03/MainActivity.java
@@ -1,5 +1,7 @@
 package tfazio.prac03;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm=getSupportFragmentManager();
+        Fragment fragMap = fm.findFragmentById(R.id.map);
+        if(fragMap == null)
+        {
+            fragMap = new Map();
+            fm.beginTransaction().add(R.id.map,fragMap).commit();
+        }
+
+        Fragment fragSelect = fm.findFragmentById(R.id.map);
+        if(fragSelect == null)
+        {
+            fragSelect = new Map();
+            fm.beginTransaction().add(R.id.map,fragSelect).commit();
+        }
+
     }
 }
