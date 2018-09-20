2eb30ceb3a25255614976313fe946c87d0ad6f06
Tristan Fazio <2tfazio@iinet.net.au>
2018-09-20 13:06:57 +0800

    Prac04

---
+++ Prac04/app/src/main/java/tfazio/prac04/FactionSchema.java
@@ -0,0 +1,16 @@
+package tfazio.prac04;
+
+public class FactionSchema
+{
+    public static class FactionTable
+    {
+        public static final String NAME = "factions";
+        public static class Cols
+        {
+            public static final String ID = "faction_id";
+            public static final String NAME = "name";
+            public static final String STREN = "strength";
+            public static final String RELAT = "relationship";
+        }
+    }
+}
