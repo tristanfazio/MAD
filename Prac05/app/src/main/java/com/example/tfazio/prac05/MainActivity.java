7202b722526f288c4026cdb5a5548ffdc9d2b579
Tristan Fazio <2tfazio@iinet.net.au>
2019-03-02 15:14:18 +0800

    cleanup

--- Prac05/app/src/main/java/com/example/tfazio/prac05/MainActivity.java
+++ Prac05/app/src/main/java/com/example/tfazio/prac05/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.tfazio.prac05;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,21 +14,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     private Intent callIntent;
     private Intent mapIntent;
     private Intent cameraIntent;
+    private Intent contactIntent;
+
     Button phoneButton;
     Button locationButton;
     Button cameraButton;
+    Button contactButton;
+
     EditText phoneEditText;
     EditText latEditText;
     EditText longEditText;
+
     ImageView cameraImageView;
 
+    TextView idTextView;
+    TextView nameTextView;
+    TextView emailTextView;
+    TextView phoneTextView;
+
     private static final int REQUEST_THUMBNAIL = 1;
+    private static final int REQUEST_CONTACT = 2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +61,12 @@ public class MainActivity extends AppCompatActivity {
         longEditText = (EditText)findViewById(R.id.longEditText);
         cameraButton = (Button)findViewById(R.id.cameraButton);
         cameraImageView = (ImageView)findViewById(R.id.cameraImageView);
+        idTextView = (TextView)findViewById(R.id.idTextView);
+        nameTextView = (TextView)findViewById(R.id.nameTextView);
+        emailTextView = (TextView)findViewById(R.id.emailTextView);
+        phoneTextView = (TextView)findViewById(R.id.phoneTextView);
+        contactButton = (Button)findViewById(R.id.contactButton);
+
         //click listeners
 
         phoneButton.setOnClickListener(new View.OnClickListener() {
@@ -85,6 +108,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        contactButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                contactIntent = new Intent(
+                        Intent.ACTION_PICK,
+                        ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(contactIntent,REQUEST_CONTACT);
+            }
+        });
+
 
     }
 
@@ -96,6 +131,35 @@ public class MainActivity extends AppCompatActivity {
         {
             Bitmap thumbnail = (Bitmap)ret.getExtras().get("data");
             cameraImageView.setImageBitmap(thumbnail);
+        }
+        else if( resultCode == Activity.RESULT_OK &&
+                    requestCode == REQUEST_CONTACT)
+        {
+            Uri contactUri = ret.getData();
+
+            String[] queryFields = new String[] {
+                    ContactsContract.Contacts._ID,
+                    ContactsContract.Contacts.DISPLAY_NAME
+            };
+
+            Cursor c = getContentResolver().query(
+                    contactUri, queryFields, null,null,null);
+            try
+            {
+                if(c.getCount()>0)
+                {
+                    c.moveToFirst();
+                    int contactId = c.getInt(0);
+                    String contactName = c.getString(1);
+
+                    idTextView.setText(Integer.toString(contactId));
+                    nameTextView.setText(contactName);
+                }
+            }
+            finally
+            {
+                c.close();
+            }
         }
     }
 }
